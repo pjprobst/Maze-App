e935b776f2e9b7d9d3bf229434d3eb390e527feb
pjprobst <pjprobst@stu.naperville203.org>
2023-10-27 13:13:14 -0500

    finished square class, created and finish square class tester

--- src/square.java
+++ src/square.java
@@ -1,18 +1,43 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-
 class square{
-    public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-
-    File file = new File("maze-1.file");
-
-    for (int i = 0; i < file.length(); i++){
-        System.out.println(file.getName());
+    private int row;
+    private int col;
+    private int type;
+    public square(int ro, int co, int typ) {
+    this.row = ro;
+    this.col = co;
+    this.type = typ;
     }
 
+    public int getRow(){
 
+        return row;
+    }
 
-}
+    public int getCol(){
+
+        return col;
+    }
+
+    public int getType(){
+
+        return type;
+    }
+
+    public String toString() {
+        if (type == 0){
+            return ("_ - empty space");
+        }
+        else if (type == 1){
+            return ("# - wall");
+        }
+        else if (type == 2){
+            return ("S - start");
+        }
+        else if (type == 3){
+            return ("E - exit");
+        }
+        else {
+            return "";
+        }
+    }
 }
