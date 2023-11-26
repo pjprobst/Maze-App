c448f54246fbcb73e452e31ee7c818bb36b2ca08
EmmaDram <esdram@stu.naperville203.org>
2023-11-26 13:05:57 -0600

    updated square class

--- src/square.java
+++ src/square.java
@@ -1,44 +1,91 @@
-class square{
+class square
+{
     private int row;
     private int col;
     private int type;
 
-    public square(int ro, int co, int typ) {
-    this.row = ro;
-    this.col = co;
-    this.type = typ;
+    private boolean explored;
+    private Square previous;
+
+    public static final int SPACE = 0;
+    public static final int WALL = 1;
+    public static final int START = 2;
+    public static final int EXIT = 3;
+    
+
+    public square(int ro, int co, int typ)
+    {
+        this.row = ro;
+        this.col = co;
+        this.type = typ;
+        this.explored = false;
+        this.previous = null;
     }
 
-    public int getRow(){
-
+    public int getRow()
+    {
         return row;
     }
 
-    public int getCol(){
-
+    public int getCol()
+    {
         return col;
     }
 
-    public int getType(){
-
+    public int getType()
+    {
         return type;
     }
 
-    public String toString() {
-        if (type == 0){
-            return ("_ - empty space");
-        }
-        else if (type == 1){
-            return ("# - wall");
-        }
-        else if (type == 2){
-            return ("S - start");
-        }
-        else if (type == 3){
-            return ("E - exit");
-        }
-        else {
-            return "";
+    public void setPrevious(Sqaure prev)
+    {
+        previous = prev;
+    }
+
+    public Square getPrevious()
+    {
+        return previous;
+    }
+
+    public boolean wasExplored()
+    {
+        return explored;
+    }
+
+    public boolean setExplored()
+    {
+        explored = true;
+    }
+
+    public void reset()
+    {
+        previous = null;
+    }
+
+    public String toString()
+    {
+        switch(type)
+        {
+            case SPACE:
+            {
+                if(explored)
+                    return ".";
+                //if is on solver work list
+                //if is on the final path to exit
+                return ("_");
+            }
+            case WALL:
+            {
+                return ("#");
+            }
+            case START:
+            {
+                return ("S");
+            }
+            case EXIT:
+            {
+                return ("E");
+            }
         }
     }
 }
